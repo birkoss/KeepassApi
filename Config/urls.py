84d244b73a8740961e5075881227a256a0e9cfb6
= <admin@birkoss.com>
2016-06-11 06:29:57 -0400

    Adding the url for the api

--- Config/urls.py
+++ Config/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-#from Keepass-Api.api import KeepassResource
+from KeepassApi.api import KeepassResource
 from KeepassApi.views import hello_world
 
-#keepass_resource = KeepassResource()
+keepass_resource = KeepassResource()
 
 urlpatterns = [
     url(r'^$', hello_world),
-#    url(r'^api/', include(keepass_resource.urls)),
+    url(r'^api/', include(keepass_resource.urls)),
     url(r'^admin/', admin.site.urls),
 ]
